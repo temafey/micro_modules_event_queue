191b780f37619580ac90df28bdfaca5933d3d298
Artem Onyschenko <a.onyshchenko@cleack.com>
2024-04-11 10:39:57 +0300

    feat(master): Add EventQueue module
    - Update QueueEventProducer, update method publishEventToQueue

--- src/Application/EventHandling/QueueEventProducer.php
+++ src/Application/EventHandling/QueueEventProducer.php
@@ -39,11 +39,19 @@ class QueueEventProducer implements QueueEventInterface
     public function publishEventToQueue(Serializable $event): void
     {
         $topicName = ($this->topic instanceof QueueEventProcessor) ? $this->topic::getTopic() : $this->topic;
+        $message = $event->serialize();
+        array_walk_recursive($message, function (&$value) {
+            if (! is_scalar($value) && $value !== null) {
+                if ($value instanceof \DateTimeInterface) {
+                    $value = $value->format(\DateTime::ATOM);
+                }
+            }
+        });
         $this->queueProducer->sendEvent(
             $topicName,
             [
                 self::FIELD_EVENT => get_class($event),
-                self::FIELD_SERIALIZED => $event->serialize()
+                self::FIELD_SERIALIZED => $message,
             ]
         );
     }
